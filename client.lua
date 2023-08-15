5e85106b5580282e667cfa89de2f4cf584466c84
Scube Scripts <104854776+ScubeScripts@users.noreply.github.com>
2023-08-15 22:57:40 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,25 @@
+CreateThread(function()
+    while true do
+        local msec = 1000;
+        local ped = PlayerPedId()
+
+
+        TriggerEvent('esx_status:getStatus', 'hunger', function(status) 
+            hunger = status.val / 10000 
+        end)
+        TriggerEvent('esx_status:getStatus', 'thirst', function(status) 
+            thirst = status.val / 10000 
+        end)
+
+        SendNUIMessage({
+            action = "UpdateHud";
+            health = GetEntityHealth(ped) - 100;
+            armour = GetPedArmour(ped);
+            hunger = hunger;
+            thirst = thirst;
+            playerId = GetPlayerServerId(PlayerId());
+        })
+
+        Wait(msec)
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'bodacious'
+game 'gta5'
+
+name "sc_hud"
+author "Scube Scripts"
+version "1.0.0"
+description "A simple and clean hud"
+
+client_scripts {
+    "client.lua"
+}
+
+server_scripts {
+    'versioncheck.lua'
+}
+
+ui_page ("ui/ui.html")
+
+files {"**/**/**/**/**/**/*.*"}
